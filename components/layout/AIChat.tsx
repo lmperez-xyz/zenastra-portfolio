dced4525f1e44ac3862c8a0ddaafb6660578af94
lmperez <azerone127@gmail.com>
2026-06-18 08:36:03 +0800

    small update on aichat

--- components/layout/AIChat.tsx
+++ components/layout/AIChat.tsx
@@ -58,7 +58,7 @@ export default function ChatPage() {
     }
 
     return (
-        <main className="mx-auto flex h-screen max-w-3xl flex-col p-6">
+        <main className="mx-auto flex h-dvh max-w-3xl flex-col p-4">
             <div className='flex flex-col items-center justify-center mb-6'>
                 <h1 className="mt-10 text-3xl">
                     Portfolio AI Assistant
